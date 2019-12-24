0aa8f7043b2ad07eb01b0b1ca8e368d43e8b30b9
kutluToren <>
2019-12-24 21:58:11 +0100

    users.test file updated with more tests

--- src/users.test.ts
+++ src/users.test.ts
@@ -19,6 +19,9 @@ var dbUser: UserHandler
   describe('#get', function () {
     it('should save the user and get the value back correctly', function (next) {
         const UserTest= new User("mockUser","mockEmail@gmail.com","1234",false)
+        const UserTest2= new User("mockUser2","mockEmail2@gmail.com","1234",false)
+        dbUser.save(UserTest2,function (err2: Error|null){})
+        
         dbUser.save(UserTest,function (err1: Error|null){
             dbUser.get("mockUser", function (err: Error | null, result?: User|undefined) {
     
@@ -29,7 +32,7 @@ var dbUser: UserHandler
             expect(Array.isArray(result))
             expect(result).to.deep.equal((UserTest))
 
-        next();
+        next()
       })
     })
     })
@@ -49,11 +52,26 @@ var dbUser: UserHandler
             //console.log(result)
             expect(err).to.be.null
             expect(result).to.not.be.undefined
+            expect(result).to.have.lengthOf(2) //There is 2 saves done in the tests
             next();
           })
         })
 
+        //Checks if delete does not executed when wrong id given
+        it('should not delete the user', function (next) {
+          dbUser.delete("wrongId", function (err: Error | null) {
+      
+          dbUser.getAll( function (err: Error | null, result?: any) {
+              //console.log("I am the result after delete",result)  
+          expect(err).to.be.null
+          expect(result).to.not.be.undefined
+          expect(result).to.not.be.empty
+          next();
+          })
+          })     
+        })
 
+        //mockUser added in previous tests 
         it('should delete the user', function (next) {
             dbUser.delete("mockUser", function (err: Error | null) {
         
@@ -61,9 +79,25 @@ var dbUser: UserHandler
                 //console.log("I am the result after delete",result)  
             expect(err).to.be.null
             expect(result).to.not.be.undefined
+            expect(result).to.have.length(1)//1 of 2 user deleted
             next();
             })
         })     
-    })
+        })
+
+        it('should delete the user', function (next) {
+          dbUser.delete("mockUser2", function (err: Error | null) {
+      
+          dbUser.getAll( function (err: Error | null, result?: any) {
+              //console.log("I am the result after delete",result)  
+          expect(err).to.be.null
+          expect(result).to.not.be.undefined
+          expect(result).to.be.empty//2 of 2 user deleted
+          next();
+          })
+      })     
+      })
+
+
   })
 })
